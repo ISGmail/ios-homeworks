c5fb2e9b701cb3252157dd548ee1bcf690680cbf
ISGmail <isgmail@mail.ru>
2022-04-19 15:26:20 +0900

    homework2

--- Navigation/Navigation/AppDelegate.swift
+++ Navigation/Navigation/AppDelegate.swift
@@ -13,7 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+    /*    let tabCtrl = UITabBarController()
+        
+        let profileView = ProfileViewController()
+        let bandView    = BandViewController()
+        
+        let bandNaviCtrl = UINavigationController(rootViewController: bandView)
+        let profileViewCtrl = UINavigationController(rootViewController: profileView)
+        
+        
+        tabCtrl.setViewControllers([bandNaviCtrl, profileViewCtrl], animated: true)*/
+        
         return true
     }
 
